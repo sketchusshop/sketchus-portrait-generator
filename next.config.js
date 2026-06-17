13fef05f619d2dbf9c1418cccbe1a04c6bac9745
sketchusshop <sketchus.shop@gmail.com>
2026-06-17 16:07:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          // Cho phép sketchus.de nhúng qua iframe
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://sketchus.de',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://sketchus.de https://*.myshopify.com",
+          },
+        ],
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;
